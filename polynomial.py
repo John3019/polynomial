88c1805f3d23d9c84d43147ead42e54d21b21400
john <jsalloumjr@hvc.rr.com>
2024-01-28 19:32:36 -0500

    "cs 506 exercise part c"

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, value):
+        return value
 
 class Int:
     def __init__(self, i):
@@ -11,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,6 +26,9 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
     
+    def evaluate(self, value):
+        return self.p1.evaluate(value) + self.p2.evaluate(value)
+    
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -27,6 +36,9 @@ class Sub:
 
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) - self.p2.evaluate(value)
 
 
 
@@ -44,6 +56,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
     
+    def evaluate(self, value):
+        return self.p1.evaluate(value) * self.p2.evaluate(value)
+    
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -57,11 +72,27 @@ class Div:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
+    
+    def evaluate(self, value):
+        denominator_value = self.p2.evaluate(value)
+        #Catch divide by 0 error
+        if denominator_value == 0:
+            raise ValueError("Division by zero is undefined.")
+        return float(self.p1.evaluate(value)) / denominator_value
 
 
-
+#Givem check
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(-1))
 
-poly = Add( Sub( Int(4), Int(3)), Add( X(), Div( Int(1), Add( Div(X(), X()), Int(1)))))
-print(poly)
+#check to ensure X is working correctly
+poly2 = Add(Int(2), X())
+print(poly2.evaluate(4))
+
+#check to ensure that division is correct and uses floats
+poly3 = Div(Int(10), X())
+print(poly3.evaluate(4))
+
+
+
